fd23d1b98ffe7608f2532d6c70ba41ddb660bdf6
puhrome <phirom.yim@fullsail.edu>
2014-05-15 14:59:32 -0400

    Declaring Variables
    List of rules and conventions on declaring variables in JS using
    WebStorm

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Declaring Variables Created by Phirom Yim on 5/15/14.
+ */
+//declaring variables using the var key words
+//declaring only happens once and only once
+//has to be done before you do anything
+//rules to follow
+//avoid special characters like *%$ etc
+// use only letters, numbers and underscores
+//do not begin with numbers
+
+
+//----Conventions-------
+//conventions don't affect how things function but important to follow
+//affects readability and professional look of code
+//names that are relevant and meaningful
+//use "camelCase"
+//begin with lower case letters
+
+//examples
+
+var yearBorn = 1955; //yearBorn is camelCase
+
+console.log(yearBorn):
